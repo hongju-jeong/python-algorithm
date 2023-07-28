f535eb67a31548806c432ee413ba226203054305
HongJu Jeong <sub06038@naver.com>
2023-07-28 15:18:45 +0900

    fibo_topdown_memoization

---
+++ DP/P2747_피보나치수_TopDown.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+D = [-1]*(N+1)
+D[0] = 0
+D[1] = 1
+
+def fibo(n):
+    if D[n] != -1:
+        return D[n]
+    
+    D[n] = fibo(n-2) + fibo(n-1)
+    return D[n]
+
+fibo(N)
+print(D[N])
