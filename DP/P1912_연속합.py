8f9bd4a1a06f63b3b30961c567217f62a4cac45d
HongJu Jeong <sub06038@naver.com>
2023-08-01 22:06:45 +0900

    1912번 연속합

---
+++ DP/P1912_연속합.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+D = [0]*(N)
+P = [0]*(N)
+
+num = input().split()
+for i in range(0,N):
+    P[i] = int(num[i])
+
+for i in range(N-1,-1,-1):
+    if(i == N-1):
+        D[i] = P[i]
+    else:
+        D[i] = max(D[i+1]+P[i],P[i])
+
+print(max(D))
+
