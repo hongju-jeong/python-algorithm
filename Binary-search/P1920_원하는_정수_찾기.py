bede472c147055f3e750a70cd4e81aa19776e862
HongJu Jeong <sub06038@naver.com>
2023-08-13 21:21:13 +0900

    1920 이분탐색 원하는 정수 찾기

---
+++ Binary-search/P1920_원하는_정수_찾기.py
@@ -0,0 +1,26 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+A = sorted(A)
+
+
+def binary_search(arr: list, target : int, start, end):
+    while start<=end:
+        pivot = (start + end)//2
+
+        if (target == arr[pivot]):
+            return 1
+        elif target < arr[pivot]:
+            end = pivot-1
+        elif target > arr[pivot]:
+            start = pivot+1
+
+    return 0
+
+for i in B:
+    print(binary_search(A, i,0,len(A)-1))
