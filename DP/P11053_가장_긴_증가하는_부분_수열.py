c5665483696c87afad6c3903e89ec3e93bf0f912
HongJu Jeong <sub06038@naver.com>
2023-08-15 00:17:42 +0900

    DP 11053 가장 긴 증가하는 부분 수열

---
+++ DP/P11053_가장_긴_증가하는_부분_수열.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+D = [0]*(N)
+P = list(map(int, input().split()))
+D[N-1] = 1
+for i in range(N-2,-1,-1):
+    big = 0
+    if(P[i]>P[i+1] or P[i] < P[i+1]):
+        for j in range(i,N):
+            if(P[i]<P[j] and big < D[j]):
+                big = D[j]
+        D[i] = big+1
+    else:
+        D[i] = D[i+1]
+
+
+print(max(D))
+
+        
