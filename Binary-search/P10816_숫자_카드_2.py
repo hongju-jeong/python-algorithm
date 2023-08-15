e8ffce56c735b24be2321fd7a820984e402bc2a0
HongJu Jeong <sub06038@naver.com>
2023-08-15 15:27:32 +0900

    BS 10816 숫자 카드 2

---
+++ Binary-search/P10816_숫자_카드_2.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+NList = list(map(int,input().split()))
+M = int(input())
+MList = list(map(int,input().split()))
+
+def binary_search(arr, target, _start, _end):
+    start = _start
+    end = _end
+    leftmost = -1
+    rightmost = -1
+    while(start<=end):
+        pivot = (start+end)//2
+        if(target == arr[pivot]): 
+            leftmost = pivot
+            end = pivot-1
+        elif(target > arr[pivot]):
+            start = pivot+1
+        else:
+            end = pivot-1
+
+    start = _start
+    end = _end
+    
+    while(start<=end):
+        pivot = (start+end)//2
+        if(target == arr[pivot]):
+            rightmost = pivot
+            start = pivot+1
+        elif(target > arr[pivot]):
+            start = pivot+1
+        else:
+            end = pivot-1
+    if(leftmost != -1 and rightmost != -1):
+        return rightmost-leftmost+1
+    else:
+        return 0
+
+NList = sorted(NList)
+
+for MNumber in MList:
+    print(binary_search(NList,MNumber,0,N-1),end=' ')
