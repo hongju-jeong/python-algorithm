62e549d42c16cfd8a35e2ae605de3065c446e536
HongJu Jeong <sub06038@naver.com>
2023-08-17 21:58:26 +0900

    BS 2792 보석 상자

---
+++ Binary-search/P2792_보석_상자.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+
+jewelryList = [0]*M
+for i in range(M):
+    jewelryList[i] = int(input())
+
+start = 1
+end = max(jewelryList)
+
+while(start<=end):
+    pivot = (start+end)//2
+
+    divideNum = 0 
+    for jewelry in jewelryList:
+        if(jewelry%pivot == 0):
+            divideNum = divideNum + (jewelry//pivot)
+        else:
+            divideNum = divideNum + (jewelry//pivot) + 1
+    if(divideNum > N):
+        start = pivot+1
+    else:
+        end = pivot-1
+
+print(start)
+
