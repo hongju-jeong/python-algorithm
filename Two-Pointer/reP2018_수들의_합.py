26e388c58bae71efde1d7c9a67e3a4b22615078a
HongJu Jeong <sub06038@naver.com>
2023-08-23 22:29:09 +0900

    TP 2018 수들의 합 다시

---
+++ Two-Pointer/reP2018_수들의_합.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+start = 1
+end = 1
+sum = 1
+count = 0
+while(end<=N):
+    if(sum < N):
+        end +=1
+        sum += end
+    elif(sum > N):
+        sum -= start
+        start +=1
+    else:
+        count += 1
+        end +=1
+        start +=1
+        sum = sum + end
+
+print(count)
