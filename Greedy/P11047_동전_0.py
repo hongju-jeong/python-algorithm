53d5b4356e97a278ee5ba33293f6d6296b40cf9f
HongJu Jeong <sub06038@naver.com>
2023-08-27 19:19:29 +0900

    Greedy 11047 동전 0

---
+++ Greedy/P11047_동전_0.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int,input().split())
+
+coins = []
+for i in range(N):
+    coin = int(input())
+    coins.append(coin)
+count = 0
+while(K > 0):
+    minIndex, minQuo = 0 , 99999999
+    for i in range(N):
+        if(K//coins[i] < minQuo and K//coins[i] > 0):
+            minIndex = i
+            minQuo = K//coins[i]
+    K = K- (coins[minIndex]*minQuo)
+    count += minQuo
+
+print(count)
+
+
